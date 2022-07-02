17058bf6f5082761bba39255b2303e7de5e71682
Lawliet <531672245@qq.com>
2022-07-02 20:02:54 +0800

    注释提交

--- packages/lm-loading/src/main.vue
+++ packages/lm-loading/src/main.vue
@@ -2,7 +2,7 @@
   <div class="lm-loading">
     <div class="loading">
       <div class="loading-inner">
-        <div class="loading-line-wrap">
+        <!-- <div class="loading-line-wrap">
           <div class="loading-line"></div>
         </div>
         <div class="loading-line-wrap">
@@ -17,7 +17,7 @@
         <div class="loading-line-wrap">
           <div class="loading-line"></div>
         </div>
-      </div>
+      </div> -->
     </div>
     <div class="loading-text">{{ loadingText }}</div>
   </div>
