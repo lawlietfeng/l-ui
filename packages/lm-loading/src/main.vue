d9b04eb3fbfa0f65f279084ae1076646b1d713b6
Lawliet <531672245@qq.com>
2022-07-02 20:36:52 +0800

    ma tijiao

--- packages/lm-loading/src/main.vue
+++ packages/lm-loading/src/main.vue
@@ -2,21 +2,21 @@
   <div class="lm-loading">
     <div class="loading">
       <div class="loading-inner">
+        <div class="loading-line-wrap">
+          <div class="loading-line"></div>
+        </div>
+        <div class="loading-line-wrap">
+          <div class="loading-line"></div>
+        </div>
         <!-- <div class="loading-line-wrap">
           <div class="loading-line"></div>
         </div>
         <div class="loading-line-wrap">
           <div class="loading-line"></div>
+        </div>
+        <div class="loading-line-wrap">
+          <div class="loading-line"></div>
         </div> -->
-        <div class="loading-line-wrap">
-          <div class="loading-line"></div>
-        </div>
-        <div class="loading-line-wrap">
-          <div class="loading-line"></div>
-        </div>
-        <div class="loading-line-wrap">
-          <div class="loading-line"></div>
-        </div>
       </div>
     </div>
     <div class="loading-text">{{ loadingText }}</div>
